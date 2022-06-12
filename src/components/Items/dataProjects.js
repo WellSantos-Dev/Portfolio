f7e5ad08dcd202265e8915ec14bc3184c402a0e7
Wellington Santos <wellsantosprofissional.dev@gmail.com>
2022-06-12 10:13:42 -0300

    Adicionando projeto testimonial

--- src/components/Items/dataProjects.js
+++ src/components/Items/dataProjects.js
@@ -1,5 +1,6 @@
 import imageBirthday from '../../assets/project-photo/simpleProjects/birthday-list.png'
 import excludeCandidates from '../../assets/project-photo/simpleProjects/exclude.png'
+import testimonials from '../../assets/project-photo/simpleProjects/testimonials.png'
 import imageDogs from '../../assets/project-photo/dogs.png'
 import imageBikcraft from '../../assets/project-photo/bikcraft.png'
 
@@ -18,17 +19,23 @@ export default [
       url: 'https://exclude-candidates.vercel.app/',
       image: `${excludeCandidates}`,
     },
+    {
+      id: 3,
+      name: 'Testimonials',
+      url: 'https://testimonial-eight.vercel.app/',
+      image: `${testimonials}`,
+    },
   ],
     // PROJETOS GRANDES
   [
     {
-      id: 3,
+      id: 4,
       name: 'Rede Social - Dogs',
       url: 'https://redesocialdogs.vercel.app/',
       image: `${imageDogs}`,
     },
     {
-      id: 4,
+      id: 5,
       name: 'Bikcraft | Em desenvolvimento',
       url: 'https://bikcraft-redesign.vercel.app/',
       image: `${imageBikcraft}`,
