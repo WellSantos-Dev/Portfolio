3611b42b58880bc1d3ce19f5c3ae5e6488e1bf8a
Wellington Santos <wellsantosprofissional.dev@gmail.com>
2022-06-14 16:30:07 -0300

    Adicionando projeto de selecionar candidato

--- src/components/Items/dataProjects.js
+++ src/components/Items/dataProjects.js
@@ -3,6 +3,7 @@ import excludeCandidates from '../../assets/project-photo/simpleProjects/exclude
 import testimonials from '../../assets/project-photo/simpleProjects/testimonials.png'
 import questions from '../../assets/project-photo/simpleProjects/questions.png'
 import cardapio from '../../assets/project-photo/simpleProjects/cardapio.png'
+import candidato from '../../assets/project-photo/simpleProjects/candidato.png'
 import imageDogs from '../../assets/project-photo/dogs.png'
 import imageBikcraft from '../../assets/project-photo/bikcraft.png'
 import devfront from '../../assets/project-photo/devfront.png'
@@ -40,23 +41,29 @@ export default [
       url: 'https://cardapio-olive.vercel.app/',
       image: `${cardapio}`,
     },
+    {
+      id: 6,
+      name: 'Informações no clique',
+      url: 'https://candidato.vercel.app/',
+      image: `${candidato}`,
+    },
   ],
     // PROJETOS GRANDES
   [
     {
-      id: 6,
+      id: 7,
       name: 'Rede Social - Dogs',
       url: 'https://redesocialdogs.vercel.app/',
       image: `${imageDogs}`,
     },
     {
-      id: 7,
+      id: 8,
       name: 'Bikcraft | Em desenvolvimento',
       url: 'https://bikcraft-redesign.vercel.app/',
       image: `${imageBikcraft}`,
     },
     {
-      id: 8,
+      id: 9,
       name: 'DevFront | Refazendo',
       url: 'https://devfrontmaster.vercel.app/',
       image: `${devfront}`,
